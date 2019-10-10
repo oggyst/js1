b6df49b89d82adf558cc0fa2f5e11c9632e732f8
oggyst <51537934+oggyst@users.noreply.github.com>
2019-10-10 13:54:22 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -13,18 +13,12 @@ function run(max , min)
         let arrColumnNumbers = [];
         let arrMatrix = [[]];
         let intArrayAverage = 0;
-        let intArrayRowAverage = 0;
-        let intCounter = 0;
-        let intSmallest = 10000;
-        let intBiggest = 0;
-        let intBiggestIndexI, intBiggestIndexJ, intSmallestIndexI, intSmallestIndexJ;
         for (var i = 0; i < intNumberOfColumns; i++) 
         {
             arrRowNumbers[i] = (Math.floor(Math.random() * (max - min)) + min);
             intArrayAverage = intArrayAverage + arrRowNumbers[i];
         }
         intArrayAverage = intArrayAverage / arrRowNumbers.length;
-        console.log (intArrayAverage);
 
         for (var i = 0; i < intNumberOfRows; i++) 
         {
@@ -41,39 +35,9 @@ function run(max , min)
             {
                 arrMatrix [i][j] = arrRowNumbers[i] * arrColumnNumbers[j];
                 intArrayAverage = intArrayAverage +  arrMatrix [i][j];
-                document.write('<td>' +arrMatrix [i][j] + '</td>');
-                if (arrMatrix[i][j] % 2 == 0)
-                {
-                    intCounter++;
-                }
+                document.write('<td> a' + i + '*b' + j + ' = ' +arrMatrix [i][j] + '</td>');
             }
-            intArrayAverage = intArrayAverage / intNumberOfRows;
-            document.write('<td>There is '+ intCounter + ' even numbers in this colummn , and average is ' + Math.floor(intArrayAverage) + '</td>');
-            
             document.write('</tr>');
         }
-        document.write('</table>');
-        for(var i = 0; i<intNumberOfRows; i++)
-        {
-            intArrayRowAverage = 0;
-            for(var j = 0; j<intNumberOfColumns; j++)
-            {
-                intArrayRowAverage += arrMatrix[j][i];
-                if (arrMatrix[j][i] > intBiggest) 
-                {
-                    intBiggest = arrMatrix[j][i];
-                    intBiggestIndexI = i;
-                    intBiggestIndexJ = j;
-                }
-                else if (arrMatrix[j][i]<intSmallest)
-                intSmallest = arrMatrix[j][i];
-                intSmallestIndexI = i;
-                intSmallestIndexJ = j;
-            }
-            intArrayRowAverage = intArrayRowAverage / intNumberOfColumns;
-            document.write (i + 1 + '. row average is ' + Math.floor(intArrayRowAverage) + '<br>');
-        }
-        arrMatrix[intSmallestIndexJ][intSmallestIndexI] = intBiggest;
-        arrMatrix[intBiggestIndexJ][intBiggestIndexI] = intSmallest;
     }
 }
